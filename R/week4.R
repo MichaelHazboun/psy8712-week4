14606929c0871a8022f1d261dd470bf139125eb4
Michael Hazboun <hazbo002@umn.edu>
2024-02-09 23:30:17 -0600

    remembered glimpse

---
+++ R/week4.R
@@ -0,0 +1,8 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+# Data Import
+import_tbl<- read_delim("../data/week4.dat", delim="-",col_names = c("casenum","parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl)
+wide_tbl<-
